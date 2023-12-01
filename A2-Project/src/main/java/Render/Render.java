f956452f10bcb295ccf9e2e979fd8127d7a5f70b
Omaid Nasary <nasaryomaid@gmail.com>
2023-12-01 16:50:37 +0000

    Graphics Commit 1: Main Menu
    Implemented the first part of Graphics with the Main Menu (subject to change).
    
    Added a placeholder image for the main menu game title.

--- A2-Project/src/main/java/Render/Render.java
+++ A2-Project/src/main/java/Render/Render.java
@@ -7,12 +7,15 @@ import Entities.Tiles.Tile;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import Game.Game;
 import Level.Level;
@@ -39,8 +42,71 @@ public class Render extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		initUI(stage);
+		menuUI(stage);
 		game = new Game();
+	}
+
+	private Pane menuUI(Stage stage){
+		//Change once a TitleImage has been made.
+		Image userInterfaceImage = new Image("file:UserInterface/Placeholder_removeInFinal.png");
+
+		BorderPane root = new BorderPane();
+
+		//Setting up the GameTitle Image shown on the main menu.
+		ImageView imageView = new ImageView();
+		imageView.setImage(userInterfaceImage);
+		imageView.setX(10);
+		imageView.setY(10);
+
+		//Positioning on Image
+		imageView.setFitHeight(90);
+		imageView.setFitWidth(90);
+		imageView.setX(150);
+		imageView.setY(90);
+
+		root.getChildren().add(imageView);
+
+		//Setup of Canvas.
+		Canvas canvas = new Canvas(400, 400); //Temporary size (Looks good though)
+		root.setCenter(canvas);
+
+		//Title of Pane
+		stage.setTitle("Main Menu (Test)");
+
+		//HorizontalBox used to place buttons in a good position.
+		HBox centralBar = new HBox();
+		centralBar.setSpacing(15);
+		centralBar.setPadding(new Insets(210, 20, 90, 30));
+		root.setCenter(centralBar);
+
+		//Buttons + functionality
+		Button loadPlayerButton = new Button("(Init) Temp");
+		centralBar.getChildren().add(loadPlayerButton);
+		loadPlayerButton.setOnAction(e -> {
+			initUI(stage);
+		});
+
+		Button newGameButton = new Button("New Game");
+		centralBar.getChildren().add(newGameButton);
+
+
+		Button leaderboardButton = new Button("Leaderboard");
+		centralBar.getChildren().add(leaderboardButton);
+
+
+		Button quitButton = new Button("Quit");
+		centralBar.getChildren().add(quitButton);
+		quitButton.setOnAction(e -> {
+			System.exit(0);
+		});
+
+		//Final stage show.
+		Scene scene = new Scene(root, 400, 400);
+
+		stage.setScene(scene);
+		stage.show();
+
+		return root;
 	}
 
 	private void initUI(Stage stage){
