8c6baeb011c41e05eb25dd95ddf4001ca534a55a
Omaid Nasary <nasaryomaid@gmail.com>
2023-12-07 12:33:47 +0000

    Graphics Commit 5 (Finalisation of Interface)
    - Built the definitive main menu interface (test menu interface is still available to test any earlier functionalities that were developed)
    
    - Developed the Level Selector Interface (Currently assumes 3 levels  will be developed) and Profile Creation Interface.

--- A2-Project/src/main/java/Render/GraphicsHandler.java
+++ A2-Project/src/main/java/Render/GraphicsHandler.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -14,9 +15,70 @@ import javafx.stage.Stage;
 public class GraphicsHandler {
     private int windowWidth = 500;
     private int windowHeight = 500;
-    private Canvas paneCanvas = new Canvas(windowWidth,windowHeight);
+    private Canvas paneCanvas = new Canvas(windowWidth, windowHeight);
 
     public void MenuScreenUI(Stage stage){;
+        //NOTE: Change once a TitleImage has been made.
+        Image userInterfaceImage = new Image("file:UserInterface/InterimTitleImage1.png");
+
+        BorderPane root = new BorderPane();
+
+        root.minHeight(200);
+        root.minWidth(200);
+        root.maxHeight(500);
+        root.minWidth(500);
+
+        //Setting up the GameTitle Image shown on the main menu.
+        ImageView imageView = new ImageView();
+        imageView.setImage(userInterfaceImage);
+        imageView.setPreserveRatio(true);
+        imageView.maxHeight(500);
+        imageView.maxWidth(500);
+
+        //IMG Size
+        imageView.setFitHeight(150);
+        imageView.setFitWidth(190);
+
+        //IMG Positioning
+        imageView.setX(150);
+        imageView.setY(150);
+
+        root.getChildren().add(imageView);
+        root.setCenter(paneCanvas);
+
+        //PANE Title
+        stage.setTitle("Main Menu");
+
+        //HorizontalBox used to place buttons in a good position.
+        HBox centralBar = new HBox();
+        centralBar.setSpacing(10);
+        centralBar.setPadding(new Insets(300, 3, 90, 120));
+        root.setCenter(centralBar);
+
+        Button newPlayerGameButton = new Button("Create a new Profile");
+        newPlayerGameButton.setOnAction(e -> {
+            CreateNewProfileUI(stage);
+        });
+
+        Button loadPreviousPlayerButton = new Button("Load Profile");
+        loadPreviousPlayerButton.setOnAction(e -> {
+            LoadProfileSelectorUI(stage);
+        });
+
+
+        Button quitButton = new Button("Quit");
+        quitButton.setOnAction(e -> {
+            System.exit(0);
+        });
+
+        centralBar.getChildren().addAll(newPlayerGameButton, loadPreviousPlayerButton, quitButton);
+
+        Scene scene = new Scene(root, windowWidth, windowHeight);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void TestScreenUI(Stage stage){;
         //NOTE: Change once a TitleImage has been made.
         Image userInterfaceImage = new Image("file:UserInterface/InterimTitleImage1.png");
 
@@ -57,7 +119,7 @@ public class GraphicsHandler {
         Button profileSelectScreenInterfaceButton = new Button("Profile Select Interface");
         profileSelectScreenInterfaceButton.setOnAction(e -> {
             //Call Profile Selection Interface for testing.
-            ProfileSelectorUI(stage);
+            LoadProfileSelectorUI(stage);
         });
 
         Button winScreenInterfaceButton = new Button("Win Screen Interface");
@@ -158,7 +220,7 @@ public class GraphicsHandler {
         stage.show();
     }
 
-    public void ProfileSelectorUI(Stage stage) {
+    public void LoadProfileSelectorUI(Stage stage) {
         BorderPane root = new BorderPane();
         stage.setTitle("Profile Selector");
 
@@ -172,11 +234,11 @@ public class GraphicsHandler {
         //VBox Declaration
         VBox centralVBox = new VBox();
         centralVBox.setPadding(new Insets(50, 3, 90, 200));
-        centralVBox.setSpacing(50);
+        centralVBox.setSpacing(0);
 
         HBox centralBar = new HBox();
-        centralBar.setSpacing(50);
-        centralBar.setPadding(new Insets(300, 3, 90, 30));
+        centralBar.setSpacing(10);
+        centralBar.setPadding(new Insets(150, 3, 90, 75));
         root.setBottom(centralBar);
 
         Button selectProfileButton = new Button("Select Profile");
@@ -195,11 +257,106 @@ public class GraphicsHandler {
             MenuScreenUI(stage);
         });
 
+        selectProfileButton.setOnAction(e ->{
+            LevelSelectorUI(stage);
+        });
+
         centralBar.getChildren().addAll(selectProfileButton, deleteProfileButton, returnToMainMenuButton);
 
         centralVBox.getChildren().add(profileSelectorInformationDisplay);
 
         root.getChildren().add(centralVBox);
+
+        Scene scene = new Scene (root, windowWidth, windowHeight);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Create New Profile
+    public void CreateNewProfileUI(Stage stage) {
+        BorderPane root = new BorderPane();
+        stage.setTitle("Create New Profile");
+
+        Label profileSelectorInformationDisplay = new Label("Please enter a name:");
+        profileSelectorInformationDisplay.setMinWidth(150);
+
+        TextField nameEntryField = new TextField();
+        nameEntryField.setMinWidth(150);
+
+        //VBox Declaration
+        VBox centralVBox = new VBox();
+        centralVBox.setPadding(new Insets(100, 3, 90, 150));
+        centralVBox.setSpacing(0);
+
+        HBox centralBar = new HBox();
+        centralBar.setSpacing(10);
+        centralBar.setPadding(new Insets(300, 3, 90, 120));
+        root.setBottom(centralBar);
+
+        Button selectProfileButton = new Button("Start");
+        selectProfileButton.setPrefWidth(Control.USE_COMPUTED_SIZE);
+        selectProfileButton.setMinWidth(100);
+
+        //Button to return to main menu:
+        Button returnToMainMenuButton = new Button("Return to Main menu");
+        returnToMainMenuButton.setMinWidth(100);
+
+        returnToMainMenuButton.setOnAction(e -> {
+            MenuScreenUI(stage);
+        });
+
+        centralBar.getChildren().addAll(selectProfileButton, returnToMainMenuButton);
+
+        centralVBox.getChildren().addAll(profileSelectorInformationDisplay, nameEntryField);
+
+        root.getChildren().add(centralVBox);
+
+        Scene scene = new Scene (root, windowWidth, windowHeight);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Level Selector
+    public void LevelSelectorUI(Stage stage) {
+        BorderPane root = new BorderPane();
+        stage.setTitle("Level Selector");
+
+        Label selectLevelLabel = new Label("Please select a level: ");
+        selectLevelLabel.setMinWidth(500);
+
+        HBox selectBar = new HBox();
+        selectBar.setSpacing(0);
+        selectBar.setPadding(new Insets(100, 3, 90, 125));
+        root.setBottom(selectBar);
+
+        HBox levelBar = new HBox();
+        levelBar.setSpacing(10);
+        levelBar.setPadding(new Insets(150, 3, 90, 125));
+        root.setBottom(levelBar);
+
+        HBox centralBar = new HBox();
+        centralBar.setSpacing(0);
+        centralBar.setPadding(new Insets(windowWidth/2, 3, 90, 190));
+        root.setBottom(centralBar);
+
+        Button levelOneButton = new Button("1");
+        Button levelTwoButton = new Button("2");
+        Button levelThreeButton = new Button("3");
+        Button randomlevelButton = new Button("R");
+
+        //Button to return to main menu:
+        Button returnToMainMenuButton = new Button("Return to Main menu");
+        returnToMainMenuButton.setMinWidth(100);
+
+        returnToMainMenuButton.setOnAction(e -> {
+            MenuScreenUI(stage);
+        });
+
+        centralBar.getChildren().addAll(returnToMainMenuButton, selectLevelLabel);
+        levelBar.getChildren().addAll(levelOneButton, levelTwoButton, levelThreeButton, randomlevelButton);
+        selectBar.getChildren().add(selectLevelLabel);
+
+        root.getChildren().addAll(selectBar, levelBar);
 
         Scene scene = new Scene (root, windowWidth, windowHeight);
         stage.setScene(scene);
