7fa786e84798300e107f6a226afbd941afbad75f
Nattyb21 <151524410+Nattyb21@users.noreply.github.com>
2023-12-09 17:52:36 +0000

    Added JavaDoc

--- A2-Project/src/main/java/Entities/Entity.java
+++ A2-Project/src/main/java/Entities/Entity.java
@@ -4,48 +4,103 @@ import Enum.EntityType;
 import Enum.Direction;
 import Level.Level;
 
+
 public abstract class Entity {
 	private boolean visible = false;
 	private EntityType type;
 	private int x;
 	private  int y;
 
+	/**
+	 * Constructs an entity with a specific type and coordinates
+	 *
+	 * @param x    The x-coordinate of the entity
+	 * @param y    The y-coordinate of the entity
+	 * @param type The type of the entity
+	 */
 	public Entity(int x, int y, EntityType type){
 		this.x = x;
 		this.y = y;
 		this.type = type;
 	}
 
+	/**
+	 *  Tick function
+	 *
+	 * @param level The current level in the game
+	 * @return Updated game level after a tick
+	 */
 	public abstract Level tick(Level level);
 
+	/**
+	 * Gets the current y-coordinate
+	 *
+	 * @return The y-coordinate
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Gets the current x-coordinate
+	 *
+	 * @return The x-coordinate
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Sets the new y-coordinate
+	 *
+	 * @return The new y-coordinate
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
+	/**
+	 * Sets the new x-coordinate
+	 *
+	 * @return The new x-coordinate
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Gets the type of the entity
+	 *
+	 * @return The type of that entity
+	 */
 	public EntityType getType() {
 		return type;
 	}
 
+	/**
+     * Checks if the entity is visible
+	 *
+	 * @return true if the entity is visible, otherwise false
+     */
 	public boolean isVisible() {
 		return visible;
 	}
 
+	/**
+	 * Sets the visibility
+	 *
+	 * @param visible true, making the entity visible, otherwise false
+	 */
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
 
+	/**
+	 * Gets the inverse direction of the current direction
+	 *
+	 * @param d The input direction
+	 * @return The inverse direction
+	 */
 	public Direction getInverse(Direction d){
 		if (d == Direction.UP){
 			return Direction.DOWN;
@@ -58,6 +113,12 @@ public abstract class Entity {
 		}
 	}
 
+	/**
+	 * Gets the clockwise direction of the current direction
+	 *
+	 * @param d The input direction
+	 * @return The clockwise direction
+	 */
 	public Direction getClockwise(Direction d){
 		if (d == Direction.UP){
 			return Direction.RIGHT;
@@ -70,6 +131,12 @@ public abstract class Entity {
 		}
 	}
 
+	/**
+	 * Gets the anti-clockwise direction of the current direction
+	 *
+	 * @param d The input direction
+	 * @return The anti-clockwise direction
+	 */
 	public Direction getAntiClockwise(Direction d){
 		if (d == Direction.UP){
 			return Direction.LEFT;
