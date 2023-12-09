ff6413c94c0abede34843793aae2d759b1461e6c
Nattyb21 <151524410+Nattyb21@users.noreply.github.com>
2023-12-09 20:00:58 +0000

    Added JavaDoc

--- A2-Project/src/main/java/Entities/Tiles/ChipSocket.java
+++ A2-Project/src/main/java/Entities/Tiles/ChipSocket.java
@@ -7,11 +7,24 @@ public class ChipSocket extends Tile{
 	private int numOfKeysRequired;
 	private boolean locked = true;
 
+	/**
+	 * Creates a chip socket with coordinates and the number of keys required to unlock it
+	 *
+	 * @param x                  The x-coordinate of the chip socket
+	 * @param y                  The y-coordinate of the chip socket
+	 * @param numOfKeysRequired The number of keys required to unlock the chip socket
+	 */
 	public ChipSocket(int x, int y, int numOfKeysRequired) {
 		super(x, y, EntityType.CHIP_SOCKET);
 		this.numOfKeysRequired = numOfKeysRequired;
 	}
-
+	/**
+	 * logic for the chip socket during each game tick
+	 * If the chip socket is unlocked, it changes to a path tile
+	 *
+	 * @param level The current level in-game
+	 * @return The updated game level after the chip socket's tick
+	 */
 	@Override
 	public Level tick(Level level) {
 		if (!locked) {
@@ -19,7 +32,11 @@ public class ChipSocket extends Tile{
 		}
 		return level;
 	}
-
+	/**
+	 * Gets the number of keys required to unlock the chip socket
+	 *
+	 * @return The number of keys required to unlock the chip socket
+	 */
 	public int getNumOfKeysRequired () {
 		return  numOfKeysRequired;
 	}
