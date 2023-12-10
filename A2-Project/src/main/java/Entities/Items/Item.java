12e83524220c25eb08b91b8cb9490e2f64e07de3
Nattyb21 <151524410+Nattyb21@users.noreply.github.com>
2023-12-10 16:41:33 +0000

    Added JavaDoc

--- A2-Project/src/main/java/Entities/Items/Item.java
+++ A2-Project/src/main/java/Entities/Items/Item.java
@@ -13,6 +13,13 @@ public class Item extends Entity {
 	public Item(int x, int y, EntityType type){
 		super(x,y,type);
 	}
+	/**
+	 * Logic for the item during each tick
+	 * If the player is on the same position as the item, it is picked up by the player and removed from the item list
+	 *
+	 * @param level The current level in-game
+	 * @return The updated game level after the tick
+	 */
 
 	@Override
 	public Level tick(Level level) {
@@ -23,19 +30,35 @@ public class Item extends Entity {
 		}
 		return level;
 	}
-
+	/**
+	 * Gets the x-coordinate of the item
+	 *
+	 * @return The new x-coordinate of the item
+	 */
 	public int getX() {
 		return super.getX();
 	}
-
+	/**
+	 * Sets the x-coordinate of the item
+	 *
+	 * @param x The new x-coordinate of the item
+	 */
 	public void setX(int x) {
 		super.setX(x);
 	}
-
+	/**
+	 * Sets the x-coordinate of the item
+	 *
+	 * @param y The new x-coordinate of the item
+	 */
 	public void setY(int y) {
 		super.setY(y);
 	}
-
+	/**
+	 * Gets the y-coordinate of the item
+	 *
+	 * @return The new y-coordinate of the item
+	 */
 	public int getY() {
 		return super.getY();
 	}
