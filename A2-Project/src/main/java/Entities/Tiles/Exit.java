e61b5850ee02c11b9b3e305b4f8fd7afa63e70bc
Nattyb21 <151524410+Nattyb21@users.noreply.github.com>
2023-12-10 16:22:44 +0000

    Added JavaDoc

--- A2-Project/src/main/java/Entities/Tiles/Exit.java
+++ A2-Project/src/main/java/Entities/Tiles/Exit.java
@@ -9,7 +9,13 @@ public class Exit extends Tile{
 	public Exit(int x, int y) {
 		super(x, y, EntityType.EXIT);
 	}
-
+	/**
+	 * Logic for the exit tile during each tick
+	 * If the player is on the exit tile, it sets the player's win status to true
+	 *
+	 * @param level The current level in-game
+	 * @return The updated game level after each tick
+	 */
 	@Override
 	public Level tick(Level level) {
 		Player p = level.getPlayer();
