08850831d00e32c623e6873d9f06067344642706
Nattyb21 <151524410+Nattyb21@users.noreply.github.com>
2023-12-10 15:54:05 +0000

    Added JavaDoc

--- A2-Project/src/main/java/Entities/Tiles/LockedDoor.java
+++ A2-Project/src/main/java/Entities/Tiles/LockedDoor.java
@@ -6,12 +6,25 @@ import Level.Level;
 public class LockedDoor extends Tile{
 	private String doorColour;
 	private boolean locked = true;
-
+	/**
+	 * Creates a locked door with the coordinates and door colour
+	 *
+	 * @param x          The x-coordinate of the locked door
+	 * @param y          The y-coordinate of the locked door
+	 * @param doorColour The colour of the locked door
+	 */
 	public LockedDoor(int x, int y, String doorColour) {
 		super(x, y, EntityType.LOCKED_DOOR);
 		this.doorColour = doorColour;
 	}
 
+	/**
+	 * Logic for the locked door during each tick
+	 * If the locked door is unlocked, it changes to a path tile
+	 *
+	 * @param level The current level in-game
+	 * @return The updated game level after the tick
+	 */
 	@Override
 	public Level tick(Level level) {
 		if (!locked) {
@@ -19,7 +32,11 @@ public class LockedDoor extends Tile{
 		}
 		return level;
 	}
-
+	/**
+	 * Gets the colour of the dor
+	 *
+	 * @return The colour of the door
+	 */
 	public String getDoorColour(){
 		return doorColour;
 	}
